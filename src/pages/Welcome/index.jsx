0f1b9b4cb5cac85d9f5b3dc298ac7536e60f26b2
Jason Francisco Macas Mora <franciscomacas3@gmail.com>
2022-11-11 21:51:14 -0500

    🎨 [UPDATE] Welcome View

--- src/pages/Welcome/index.jsx
+++ src/pages/Welcome/index.jsx
@@ -1,9 +1,12 @@
+import { Link } from "react-router-dom";
 import { UilEnvelopeAlt } from '@iconscout/react-unicons';
 import { UilApple } from '@iconscout/react-unicons';
+import "./styles.css";
+
 
 export const Welcome = () => {
     return (
-        <section>
+        <section className="overflow-hidden">
             <video
                 src="https://www.shutterstock.com/shutterstock/videos/34559794/preview/stock-footage-chef-serves-salad-on-the-plate-in-restaurant.webm"
                 className="absolute top-0 left-0 object-cover h-screen w-full"
@@ -11,17 +14,21 @@ export const Welcome = () => {
                 loop="true"
                 muted
             />
-            <div className="text-darkd absolute bottom-[40%] z-10 max-w-[400px]">
-                <h3 className="text-4xl font-bold">Welcome to Daily Recipe App</h3>
+            <div className="bg-video">
+                <div className="text-darkd relative z-20 mt-[45vh] max-w-[400px] px-4 ">
+                <h3 className="text-4xl font-bold mb-4">Welcome to Daily Recipe App</h3>
                 <p className="mb-4">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, molestias?</p>
-                <button className="btn btn-primary btn-rounded mb-4">
+                <Link to="/" className="btn btn-primary btn-rounded mb-6">
                     <UilEnvelopeAlt/>
                     <p>Signup with Email</p>
-                </button>
-                <button className="btn btn-light btn-rounded">
+                </Link>
+                <Link to="/" className="btn btn-light btn-rounded">
                     <UilApple/>
                     <p>Signup with Apple</p>
-                </button>
+                </Link>
+
+                <p className="text-center mt-6 underline">Alredy have an account?</p>
+                </div>
             </div>
         </section>
     )
