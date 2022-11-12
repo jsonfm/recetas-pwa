6ca8f21640b51e197ce5945fb645dd050c4784aa
Jason Francisco Macas Mora <franciscomacas3@gmail.com>
2022-11-11 22:43:28 -0500

    🎨 [UPDATE] Explore View

--- src/pages/Explore/index.jsx
+++ src/pages/Explore/index.jsx
@@ -1,7 +1,17 @@
+
+const tags = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
 export const Explore = () => {
     return (
-        <section>
-            Explore
+        <section className="pt-12 pb-20 px-4">
+            <h4 className="mb-4 font-semibold text-xl">Explore</h4>
+            <h5>Quick Search</h5>
+            <h5 className="mb-4">Popular Tags</h5>
+            <div className="flex flex-wrap justify-center gap-4">
+                {tags.map((_) => (
+                    <div className="tag">tag name</div>
+                ))}
+            </div>
         </section>
     )
 }
